972ffafdf1b63d010ddf551f8089c8b11af55555
shaku-hussien <nadahussein686@gmail.com>
2024-12-29 20:04:28 +0300

    day 26

---
+++ main.js
@@ -0,0 +1,62 @@
+                                        // Quiz Time
+// ans 1
+
+// const waa variable hada halmar qiima u astayso aan dib wax ugu badali karin  
+// let isna waa variable xili kasta aad u baahato ayaa wax ka badali kartaa
+// var iyo let aya isku mid ah iyaga wax waad ka badali kartaa or mcnha sidaa rabto u isticmaali kartaa 
+
+
+// // ans 2
+
+// let x = 10;
+// let y = 5; 
+// let add = x + y; 
+// let minus = x - y; 
+// let mult = x * y; 
+// let divide = x / y; 
+// console.log("add: " + add); 
+// console.log("minus: " + minus); 
+// console.log("mult:" + mult);  
+// console.log("Divide: " + divide);
+
+// // ans 3
+
+// let score = 50;
+//  if (score >= 90) { 
+//     console.log("Grade A"); 
+// } 
+// else if (score >= 75 && score < 90) {
+//     console.log("Grade B"); 
+// } 
+// else if (score < 75 && score >50) {
+//     console.log("Grade C"); 
+// } 
+// else {
+//     console.log("Grade F"); 
+// }
+
+
+// ans 4
+
+// let num = 6; 
+// if (num % 2 === 0) { 
+//     console.log(num + " is even"); 
+// } else { 
+//     console.log(num + " is odd"); 
+// }
+
+
+// // ans 5
+
+// for(let kafia = 1; kafia <= 10; kafia++){
+//      console.log(kafia)
+// };
+
+
+// ans 6
+
+// let i = 10; 
+// while (i > 0) { 
+//     console.log(i); 
+//     i--;
+// }
